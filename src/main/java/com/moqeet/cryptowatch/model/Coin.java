3f9f8e352b629e2cc2022d4f9fcecf5614144767
MoqeetN <moqeet.nadeem@cleargoals.com>
2021-10-01 19:16:10 -0400

    First try at unpacking nested "quote" json property.

--- src/main/java/com/moqeet/cryptowatch/model/Coin.java
+++ src/main/java/com/moqeet/cryptowatch/model/Coin.java
@@ -1,9 +1,13 @@
 package com.moqeet.cryptowatch.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.Map;
 
+@Getter
+@Setter
 public class Coin {
 
     private String id;
@@ -19,12 +23,19 @@ public class Coin {
     private String platform;
     private String cmc_rank;
     private String last_updated;
-    private Map<String, Object> quote;
+    private String currency;
+    private String price;
+    private String percent_change_24h;
+    private String percent_change_7d;
+    private String percent_change_30d;
+    private String percent_change_60d;
+    private String percent_change_90d;
+    private String market_cap;
 
     public Coin() {
     }
 
-    public Coin(String id, String name, String symbol, String slug, String num_market_pairs, String date_added, String[] tags, String max_supply, String circulating_supply, String total_supply, String platform, String cmc_rank, String last_updated, Map<String, Object> quote) {
+    public Coin(String id, String name, String symbol, String slug, String num_market_pairs, String date_added, String[] tags, String max_supply, String circulating_supply, String total_supply, String platform, String cmc_rank, String last_updated, String price, String percent_change_24h, String percent_change_7d, String percent_change_30d, String percent_change_60d, String percent_change_90d, String market_cap) {
         this.id = id;
         this.name = name;
         this.symbol = symbol;
@@ -38,119 +49,26 @@ public class Coin {
         this.platform = platform;
         this.cmc_rank = cmc_rank;
         this.last_updated = last_updated;
-        this.quote = quote;
+        this.price = price;
+        this.percent_change_24h = percent_change_24h;
+        this.percent_change_7d = percent_change_7d;
+        this.percent_change_30d = percent_change_30d;
+        this.percent_change_60d = percent_change_60d;
+        this.percent_change_90d = percent_change_90d;
+        this.market_cap = market_cap;
     }
 
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSymbol() {
-        return symbol;
-    }
-
-    public void setSymbol(String symbol) {
-        this.symbol = symbol;
-    }
-
-    public String getSlug() {
-        return slug;
-    }
-
-    public void setSlug(String slug) {
-        this.slug = slug;
-    }
-
-    public String getNum_market_pairs() {
-        return num_market_pairs;
-    }
-
-    public void setNum_market_pairs(String num_market_pairs) {
-        this.num_market_pairs = num_market_pairs;
-    }
-
-    public String getDate_added() {
-        return date_added;
-    }
-
-    public void setDate_added(String date_added) {
-        this.date_added = date_added;
-    }
-
-    public String[] getTags() {
-        return tags;
-    }
-
-    public void setTags(String[] tags) {
-        this.tags = tags;
-    }
-
-    public String getMax_supply() {
-        return max_supply;
-    }
-
-    public void setMax_supply(String max_supply) {
-        this.max_supply = max_supply;
-    }
-
-    public String getCirculating_supply() {
-        return circulating_supply;
-    }
-
-    public void setCirculating_supply(String circulating_supply) {
-        this.circulating_supply = circulating_supply;
-    }
-
-    public String getTotal_supply() {
-        return total_supply;
-    }
-
-    public void setTotal_supply(String total_supply) {
-        this.total_supply = total_supply;
-    }
-
-    public String getPlatform() {
-        return platform;
-    }
-
-    public void setPlatform(String platform) {
-        this.platform = platform;
-    }
-
-    public String getCmc_rank() {
-        return cmc_rank;
-    }
-
-    public void setCmc_rank(String cmc_rank) {
-        this.cmc_rank = cmc_rank;
-    }
-
-    public String getLast_updated() {
-        return last_updated;
-    }
-
-    public void setLast_updated(String last_updated) {
-        this.last_updated = last_updated;
-    }
-
-    public Map<String, Object> getQuote() {
-        return quote;
-    }
-
-    public void setQuote(Map<String, Object> quote) {
-        this.quote = quote;
+    @SuppressWarnings("unchecked")
+    @JsonProperty("quote")
+    private void unpackNested(Map<String,Object> quote) {
+        Map<String,String> quoteData = (Map<String,String>)quote.get("USD");
+        this.price = quoteData.get("price");
+        this.percent_change_24h = quoteData.get("percent_change_24h");
+        this.percent_change_7d = quoteData.get("percent_change_7d");
+        this.percent_change_30d = quoteData.get("percent_change_30d");
+        this.percent_change_60d = quoteData.get("percent_change_60d");
+        this.percent_change_90d = quoteData.get("percent_change_90d");
+        this.market_cap = quoteData.get("market_cap");
     }
 
 
